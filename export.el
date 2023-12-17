2134600c0ade4673534600421d354e7df942dda9
Lucien Cartier-Tilet <lucien@phundrak.com>
2023-12-17 21:31:17 +0100

    chore(export): change from ox-gfm to built-in ox-md
    HTML tables are better supported in Vuepress than Markdown tables

--- export.el
+++ export.el
@@ -4,12 +4,11 @@
 (package-initialize)
 (package-refresh-contents)
 (package-install 'f)
-(package-install 'ox-gfm)
 
 (message "Emacs directory: %s" user-emacs-directory)
 
 (require 'f)
-(require 'ox-gfm)
+(require 'ox-md)
 (require 'ox-publish)
 
 (setq org-confirm-babel-evaluate nil
@@ -23,6 +22,6 @@
   (message "Exporting %s" (f-relative file))
   (with-temp-buffer
       (find-file file)
-      (org-export-to-file 'gfm (concat (f-no-ext file) ".md"))))
+      (org-export-to-file 'md (concat (f-no-ext file) ".md"))))
 
 (message "Project generated!")
